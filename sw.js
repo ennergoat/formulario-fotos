df63bb788172b13a950556cba59a22cd85c8d8f0
ennergoat <ennergoatvalencia2000@gmail.com>
2026-01-07 15:38:35 -0500

    cambio con el icono y funcionalidad

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
 // Service Worker para la aplicación PWA
-const CACHE_NAME = 'expediente-fotos-v1';
+const CACHE_NAME = 'expediente-fotos-v2';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
 // Instalar el Service Worker
 self.addEventListener('install', event => {
+  // Forzar que el SW espere hasta que todo se cachee, pero permitir skipWaiting manual si se requiere
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -15,21 +18,28 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
   );
+  // Forzar activación inmediata
+  self.skipWaiting();
 });
 
 // Activar el Service Worker
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Eliminando cache antigua:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    Promise.all([
+      // Limpiar caches antiguos
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE_NAME) {
+              console.log('Eliminando cache antigua:', cacheName);
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      }),
+      // Tomar control de los clientes inmediatamente
+      self.clients.claim()
+    ])
   );
 });
 
@@ -42,8 +52,27 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                // Solo cachear peticiones GET
+                if (event.request.method === 'GET') {
+                    cache.put(event.request, responseToCache);
+                }
+              });
+
+            return response;
+          }
+        );
+      })
   );
 });
